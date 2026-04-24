02f9c912b89aacd93fb2753466f6daeaac3584dc
aarpatil09-lgtm <aarpatil09@gmail.com>
2026-04-24 15:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function mincose(arr){
-	let cost =0 ;
-	arr.sort((a,b)=>a-b);
+function minCost(arr) {
+    let cost = 0;
 
-	while(arr.length>1){
-	 let first = arr.shift();
-		let second = arr.shift();
-		let sum = first + second;
-		cost = cost + sum;
+    arr.sort((a, b) => a - b);
 
-		arr.push(sum);
-	   arr.sort((a,b)=>a-b);
-	}
+    while (arr.length > 1) {
+        let first = arr.shift();
+        let second = arr.shift();
+
+        let sum = first + second;
+        cost += sum;
+
+        arr.push(sum);
+        arr.sort((a, b) => a - b);
+    }
+
+    return cost;
 }
